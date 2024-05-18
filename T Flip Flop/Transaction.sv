7f65d8ec33e445b1afd5cc99239c044fa3b5c783
Abinash200200 <160818690+Abinash200200@users.noreply.github.com>
2024-05-18 11:39:26 +0530

    Transaction.sv

---
+++ T Flip Flop/Transaction.sv
@@ -0,0 +1,6 @@
+class trans;    // Class declaration
+  bit t;        // Declare a bit type variable 't'
+  reg q;        // Declare a reg type variable 'q'
+  bit clk;      // Declare a bit type variable 'clk'
+  bit reset;    // Declare a bit type variable 'reset'
+endclass        // End of class declaration
