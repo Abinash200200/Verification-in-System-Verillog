f81205cf5379b830a51983e6310fc0a9acdf8d99
Abinash200200 <160818690+Abinash200200@users.noreply.github.com>
2024-05-24 11:15:25 +0530

    Testbench.sv

---
+++ Sync_FIFO/Testbench.sv
@@ -0,0 +1,28 @@
+`include "Enviroment.sv"
+`include "Interface.sv"
+
+module tb;
+  environment env;
+
+intf inf();
+fifo dut(inf.clk, inf.rst, inf.wr, inf.rd, inf.data_in, inf.data_out, inf.empty, inf.full);
+
+initial
+	inf.clk <= 0;
+
+always #10 inf.clk <= ~inf.clk;
+
+initial begin
+	env = new(inf);
+	env.gnt.count = 30;
+	env.run();
+end
+
+initial begin
+	$dumpfile("fifo.vcd");
+	$dumpvars(0,tb);
+  #2000
+  $finish;
+end
+
+endmodule
